dc2498a348514d7a1039fed8540d5a2ca792432d
Tom Meyer <tom.meyer89@gmail.com>
2024-04-25 11:12:48 +0200

    Update default.nix

--- modules/default.nix
+++ modules/default.nix
@@ -69,14 +69,19 @@
     ./partitioning.nix
   ];
 # TODO: Config layout
+# cluster = {
+#   k3sInit = "bob";
+#   vault.clusterAddress = "https://vault.example.com:8201";
+# };
 # node = {
-#   name = "BOB";
+#   name = "BOB"; TODO: can this name be used from the nixos configuration name?
 #   hostid = "random123";
 #   domainNames = {
 #     "example.com" = [ config.node.iface1.eno1.ipv4 ];
 #   };
 #   iface = {
 #     eno1 = {
+#       We can either reference the usage here ( usage = [ "valut" "k3s-server" ] ) or include these ips manually in the services
 #       ipv4 = "0.0.0.0";
 #       ipv6 = ::;
 #     };
